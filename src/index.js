3ec907feb7dbfa8a95ca8e3dd9d2b3df251673ff
aditya41 <aditya41@users.noreply.github.com>
2020-12-29 01:18:55 +0530

    my first

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+    socket.broadcast.emit('message', 'a new user has joined')
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+    })
+    socket.on('disconnect', () => {
+        io.emit('A user has left')
+    })
+})
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+})
